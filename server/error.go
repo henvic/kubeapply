61b39920dbd7c366ad09cb2c93283e65ea3b0671
Henrique Vicente <henriquevicente@gmail.com>
2019-03-26 23:55:22 -0300

    server/error: fixing superflous response.WriteHeader call (header already sent); renaming feedback with errors.

--- server/error.go
+++ server/error.go
@@ -9,28 +9,28 @@ import (
 )
 
 type response struct {
-	Status   int    `json:"status"`
-	Message  string `json:"message"`
-	Feedback string `json:"feedback,omitempty"`
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Errors  string `json:"errors,omitempty"`
 }
 
-// ErrorHandler for errors
-func ErrorHandler(w http.ResponseWriter, r *http.Request, code int, feedback ...string) {
-	var f = strings.Join(feedback, "\n")
-
-	w.Header().Set("Content-Type", "application/json; charset=utf8")
-	w.WriteHeader(code)
+// ErrorHandler for errors.
+// Use code = -1 to bypass writing header or trying to set Content-Type. Useful when headers are already sent.
+func ErrorHandler(w http.ResponseWriter, r *http.Request, code int, errors ...string) {
+	if code >= 0 {
+		w.Header().Set("Content-Type", "application/json; charset=utf8")
+		w.WriteHeader(code)
+	}
 
 	if code != http.StatusNotFound {
 		log.Debugf("Request error: %v (%v)", code, http.StatusText(code))
 	}
 
 	if err := json.NewEncoder(w).Encode(response{
-		Status:   code,
-		Message:  http.StatusText(code),
-		Feedback: f,
+		Status:  code,
+		Message: http.StatusText(code),
+		Errors:  strings.Join(errors, "\n"),
 	}); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
 		log.Error(err)
 	}
 }
